6a440676a8fbf25060ebc0b1d8fde0612c3a80a9
suraj agarwal <surajagarwal560@gmail.com>
2024-01-16 15:11:15 +0530

    #suraj

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,50 @@
 import express from "express";
+import generatePDF from './generatePDF';
+import fs from 'fs';
+
 const app = express();
 const port = 8000;
 
 app.get('/', (req, res) => {
     res.send("hello world");
-})
+});
+
+app.get('/generate-pdf', (req, res) => {
+    try {
+        const invoiceDetails= {
+            username: "John Doe",
+            email: "john@example.com",
+            number: "INV123",
+            itemprice: 50,
+            tax: 5,
+            totalPrice: 55,
+            id: 1,
+        };
+
+        const pdfFilePath = generatePDF(invoiceDetails);
+
+        res.sendFile(pdfFilePath, (err) => {
+            if (err) {
+                res.status(500).send('Error sending pdf');
+            }
+
+            cleanupPDF(pdfFilePath);
+        });
+    } catch (error) {
+        res.status(500).send('Error generating PDF');
+    }
+});
 
 app.listen(port, () => {
-    return console.log("express server is running on ", port);
-    
-})
+    console.log("express server is running on ", port);
+});
+
+const cleanupPDF = (pdfFilePath: string) => {
+    fs.unlink(pdfFilePath, (err) => {
+        if (err) {
+            console.error('Error deleting PDF file', err);
+        } else {
+            console.log('PDF file deleted successfully');
+        }
+    });
+};
---
+++ server/src/generatePDF.ts
@@ -0,0 +1,34 @@
+import fs from 'fs';
+import PDFDocument from 'pdfkit';
+
+interface InvoiceDetails {
+  username: string;
+  email: string;
+  number: string;
+  itemprice: number;
+  tax: number;
+  totalPrice: number;
+  id: number;
+}
+
+// Function to generate pdf based on invoice details
+const generatePDF = (invoiceDetails: InvoiceDetails): string => {
+  const doc = new PDFDocument();
+  const pdfFilePath = `server/pdfs/${invoiceDetails.id}.pdf`;
+
+  // pipe the pdf to a file
+  doc.pipe(fs.createWriteStream(pdfFilePath));
+
+  // content of pdf
+  doc.text(`Invoice Number: ${invoiceDetails.id}`);
+  doc.text(`Username: ${invoiceDetails.username}`);
+  doc.text(`Email: ${invoiceDetails.email}`);
+  doc.text(`Item Price: ${invoiceDetails.itemprice}`);
+  doc.text(`Tax: ${invoiceDetails.tax}`);
+  doc.text(`Total Price: ${invoiceDetails.totalPrice}`);
+
+  doc.end();
+  return pdfFilePath;
+};
+
+export default generatePDF;
